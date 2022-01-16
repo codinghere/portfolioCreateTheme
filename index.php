3bd7569943ce177ea869394041a905b9377a0e39
codinghere <codingaziz22@gmail.com>
2022-01-17 01:32:03 +0600

    hello

---
+++ index.php
@@ -0,0 +1,363 @@
+
+<?php get_header(); ?>
+
+  <!-- Banner Section -->
+  <section id="banner-section">
+    <div id="particles-js"></div>
+    <div class="container">
+      <div class="row ">
+        <div class="col-md-6 col-xs-12">
+          <div class="banner-main-content">
+            <div class="banner-designation">
+              <p>WordPress</p>
+              <p>UI/UX Design</p>
+              <p>Shopify</p>
+            </div>
+            <div class="banner-title">
+              <h1>I design and build website.</h1>
+            </div>
+            <div class="banner-content">
+              <p>I work with individuals and businesses to create their own unique identity on the internet. </p>
+            </div>
+            <div class="banner-content-button">
+              <button class="btn btn-outline-danger">Let's Meet</button>
+              <button class="btn btn-danger">See My Work</button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-xs-12">
+          <div class="banner-second-part">
+            <div class="banner-img">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/portfolio.png" alt="" >
+              <p>Mohammad Ismail</p>
+            </div>
+            <div class="banner-social-section">
+              <div class="middle ml-1">          
+                <a class="btn2" href="https://www.facebook.com/xyz/">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a class="btn3" href="https://twitter.com/xyz">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a class="btn4" href="https://snapchat.com/add/xyz">
+                  <i class="fab fa-whatsapp"></i>
+                </a>
+                <a class="btn5" href="https://www.instagram.com/xyz/">
+                  <i class="fab fa-skype"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Banner Section -->
+  <!-- About -->
+<section id="about" class="py-5">
+  <div class="container">
+    <div class="row">
+    <div class=""col-12 "">
+        <span>FEATURES</span><br>
+        <h3>What I Do</h3>
+    </div>
+     <div class="col-sm-12 col-md-6 col-lg-4">
+       <div class="about-box">
+         <div class="about-box-inner">
+           <div class="a-box-ic"><i class="fa fa-bars" style="color: #fff;" aria-hidden="true"></i></div>
+           <div class="a-box-content">
+             <div class="a-b-c-title"><h4 style="color: #fff;">Business Strategy</h4></div>
+             <div class="a-b-c-des"><p>I throw myself down among the tall grass by the stream as I lie close to the earth.</p></div>
+             <div class="a-b-c-read-more-btn">
+               <a href=""><span class="iconify" data-icon="feather:arrow-right" style="color: #f9004d;" data-width="32"></span></a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+    </div>
+  </div>
+</section>
+  <!-- Services Section -->
+  <section id="services">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-md-5">
+          <div class="services-title">
+            <h2>Your Website Is Your First Proposal, Make Sure It Is Appealing!</h2>
+          </div>
+          <div class="services-content">
+            <p>The world is slowly moving towards more online communication. Make sure your business doesn’t fall
+              behind. A few reasons why an updated website is important:</p>
+          </div>
+          <div class="services-content-two">
+            <p>It looks more professional than your competition</p>
+            <p>It shows that you take your business serious</p>
+            <p>It will make the sale a lot easier</p>
+          </div>
+          <!-- <div class="services-button">
+            <button class="btn btn-danger">Book Free Consultation</button>
+          </div> -->
+        </div>
+        <div class="col-md-7">
+          <div class="service-img-sec">
+            <div class="img-one ">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Group-15.jpg" alt="">
+            </div>
+            <div class="img-two">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ismailbsc.jpg" alt="">
+            </div>
+            <div class="img-three">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Group-16.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Latest Project -->
+  <section id="latest-project">
+    <div class="container">
+      <div id="particles-js-two"></div>
+      <div class="row">
+        <div class="global-title text-center">
+          <h2>View Some of My Latest
+            <span class="text-danger"> Creations For Real Clients.</span>
+          </h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-sm-12 workcontent">
+          <div class="window">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fullpage.png" alt="">
+          </div>
+          <div class="lw-link">
+            <a href=""><span><i class="fab fa-figma"></i></span> See UI/UX</a>
+            <a href=""><span><i class="fas fa-link"></i></span>https://www.sakib.com</a>
+          </div>
+          <div class="lw-livelink">
+            <a href="" class="arrow-button" target="_blank"> See more
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-sm-12 workcontent">
+          <div class="window">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fullpagetwo.png" alt="">
+          </div>
+          <div class="lw-link">
+            <a href=""><span><i class="fab fa-figma"></i></span> See UI/UX</a>
+            <a href=""><span><i class="fas fa-link"></i></span>https://www.sakib.com</a>
+          </div>
+          <div class="lw-livelink">
+            <a href="" class="arrow-button" target="_blank"> See more
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="lp-btn text-center ">
+        <button class="btn btn-danger btn-lg">More Projects</button>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <!-- Testimonial Start -->
+  <section id="testimonial">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="slider-92911">
+            <span class="fp__testimonial_quote"> <i class="flaticon-quote-left"></i></span>
+            <div class="owl-carousel slide-one-item">
+              <div class="d-md-flex testimony-29101 align-items-stretch">
+                <div class="image" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/person_1.jpg');"></div>
+                <div class="text">
+                  <blockquote>
+                    <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, at! Atque totam
+                      obcaecati veniam eius vero, similique quibusdam! Sunt sequi, nemo. Quam consequuntur ipsum
+                      suscipit repellat molestiae laboriosam, incidunt!&rdquo;</p>
+                    <div class="author">&mdash; Jean Stephen</div>
+                  </blockquote>
+                </div>
+              </div>
+              <div class="d-md-flex testimony-29101 align-items-stretch">
+                <div class="image" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/person_2.jpg');"></div>
+                <div class="text">
+                  <blockquote>
+                    <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, at! Atque totam
+                      obcaecati veniam eius vero, similique quibusdam! Sunt sequi, nemo. Quam consequuntur ipsum
+                      suscipit repellat molestiae laboriosam, incidunt!&rdquo;</p>
+
+                    <div class="author">&mdash; Julia Smith</div>
+                  </blockquote>
+                </div>
+              </div>
+              <div class="d-md-flex testimony-29101 align-items-stretch">
+                <div class="image" style="background-image: url('<?php echo get_template_directory_uri();?>/assets/img/person_2.jpg');"></div>
+                <div class="text">
+                  <blockquote>
+                    <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, at! Atque totam
+                      obcaecati veniam eius vero, similique quibusdam! Sunt sequi, nemo. Quam consequuntur ipsum
+                      suscipit repellat molestiae laboriosam, incidunt!&rdquo;</p>
+
+                    <div class="author">&mdash; Julia Smith</div>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+            <div class="my-5 text-center">
+              <ul class="thumbnail">
+                <li class="active"><a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/img/person_1_sm.jpg" alt="Image" class="img-fluid"></a></li>
+                <li><a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/img/person_2_sm.jpg" alt="Image" class="img-fluid"></a></li>
+                <li><a href="#"><img src="<?php echo get_template_directory_uri();?>/assets/img/person_3_sm.jpg" alt="Image" class="img-fluid"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Contact Section -->
+  <section class="h-100 ">
+    <div class="container py-5 h-100">
+      <div class="row">
+        <div class="global-title text-center">
+          <p>What’s In Your Mind?</p>
+          <h2>lets
+            <span class="text-danger"> Talk About.</span>
+          </h2>
+          <p>If you have a project in mind or you just want to chat, feel free to get in touch. Currently my time
+            books quickly, so the sooner you write, the better it is for both of us.</p>
+        </div>
+      </div>
+      <div class="row">
+       <div class="col-md-6 d-none d-md-block align-self-center mt-4">  <!--  -->
+          <img src="<?php echo get_template_directory_uri();?>/assets/img/Synchronous dialing.gif" alt="Sample photo" class="img-fluid"
+            style="border-radius: .25rem; "/>
+                 <div class="middle mt-4">
+                   <h3 class="text-light text-uppercase font-weight-bold">lets <span class="text-danger font-weight-bold">live chat</span></h3>
+                      <a class="btn2" href="https://www.facebook.com/xyz/">
+                           <i class="fab fa-facebook-f"></i>
+                      </a>
+       
+                      <a class="btn3" href="https://twitter.com/xyz">
+                           <i class="fab fa-twitter"></i>
+                      </a>
+
+                        <a class="btn4" href="https://snapchat.com/add/xyz">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+
+                      <a class="btn5" href="https://www.instagram.com/xyz/">
+                           <i class="fab fa-skype"></i>
+                      </a>
+
+                 </div>
+       
+        </div>
+
+        <div class="col-md-6">
+          <div class="card card-registration my-4">
+            <div class="card-body p-md-4 text-black">
+              <div class="row">
+                <div class="col-md-12 mb-4">
+                  <div class="form-outline">
+                    <label class="form-label" for="form3Example1m">Name</label>
+                    <input type="text" id="form3Example1m" class="form-control form-control-md"
+                      placeholder="Enter Your Name" />
+                  </div>
+                </div>
+                <div class="col-md-12 mb-4">
+                  <div class="form-outline">
+                    <label class="form-label" for="form3Example1m">Email</label>
+                    <input type="text" id="form3Example1m" class="form-control form-control-md"
+                      placeholder="Enter your Email" />
+                  </div>
+                </div>
+              </div>
+              <!-- <div class="form-outline mb-4">
+                    <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                    <label class="form-label" for="form3Example8">Address</label>
+                  </div> -->
+              <div class="row">
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                    <input type="text" id="form3Example1m" class="form-control form-control-md" />
+                    <label class="form-label" for="form3Example1m">First name</label>
+                  </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                    <input type="text" id="form3Example1n" class="form-control form-control-md" />
+                    <label class="form-label" for="form3Example1n">Last name</label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-4">
+                  <label class="form-label" for="form3Example1m">Your Category</label>
+
+                  <select class="form-select  form-select-md" aria-label="Default select example">
+                    <option selected>Wordpress</option>
+                    <option value="1">Web Design</option>
+                    <option value="2">Laravel</option>
+                    <option value="3">Android</option>
+                  </select>
+                </div>
+
+                <div class="col-md-6 mb-4">
+                  <div class="form-outline">
+                    <label class="form-label" for="form3Example1m">Your Budget</label>
+                    <input type="text" id="form3Example1m" class="form-control form-control-md"
+                      placeholder="Enter your budget" />
+                  </div>
+                </div>
+              </div>
+              <select class="form-select form-select-md mb-3" aria-label=".form-select-lg example">
+                <option selected>Select your country</option>
+                <option value="1">Afg</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <div class="form-group">
+                <label for="exampleFormControlTextarea3">Your Project Brief</label>
+                <textarea class="form-control" id="exampleFormControlTextarea3" rows="5"></textarea>
+              </div>
+              <div class="forn-group">
+                <!-- <label class="form-label" for="customFile">Default file input example</label> -->
+                <input type="file" class="form-control mt-4" id="customFile" />
+              </div>
+              <div class=" form-group d-flex justify-content-start mt-4">
+                <button type="button" class="btn btn-danger btn-md ">Submit order</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
